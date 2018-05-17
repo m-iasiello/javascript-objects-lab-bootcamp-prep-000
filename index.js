68c752770742a548139507d5c197719de0eead75
Michael Iasiello <maiasiel@ncsu.edu>
2018-05-17 12:32:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,10 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
   obj[key] = value
   return obj
 }
+
+function deleteFromObjectByKey(obj, key) {
+  var newObj = {}
+  Object.assign(newObj, obj)
+  delete newObj.key
+  return newObj
+}
